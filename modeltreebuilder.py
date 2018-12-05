41b145367dee73493987cf0f33b1ddd8f62556f1
Emma Hamel <ebhamel2@illinois.edu>
2018-12-05 11:38:18 -0600

    Update modeltreebuilder.py

--- modeltreebuilder.py
+++ modeltreebuilder.py
@@ -9,12 +9,10 @@ work='/projects/tallis/hamel/RAxML-vs.-FastTree/ModelTrees/'
 for i in range (0,3):
 	treeDictionaryDenseTaxa[i] = dendropy.Tree.get(file=open(work + 'Originaltree.tt'),
                 	schema="newick")
-	treeDictionaryDenseTaxa[i].scale_edges(.5)
 	treeDictionarySparceTaxa[i] = dendropy.Tree.get(file=open(work + 'Originaltree.tt'),
                 	schema="newick")
-	treeDictionarySparceTaxa[i].scale_edges(.5)
 
-setConstant = [.1, 1, 2]
+setConstant = [10, 50, 100]
 
 for k in range(0, 3):
 	treeDictionaryDenseTaxa[k].scale_edges(setConstant[k])
@@ -34,7 +32,7 @@ for l in range(0, len(treeDictionarySparceTaxa)):
 setName = ["small", "moderate", "large"]
 
 for i in range (0,3):
-	treeDictionaryDenseTaxa[i].write(path=work + setName[i] + 'lengthDense.tt', 
+	treeDictionaryDenseTaxa[i].write(path=work + setName[i] + '_dense.tt', 
 		schema="newick")
-	treeDictionarySparceTaxa[i].write(path=work + setName[i] + 'lengthSparse.tt', 
+	treeDictionarySparceTaxa[i].write(path=work + setName[i] + '_sparse.tt', 
 		schema="newick")
